e112f9e58085b5631d004b74c26cc507ff5c8bf6
Richard Hunter <richard@richardhunter.co.uk>
2016-08-21 19:10:13 +0100

    paths

--- bundle.js
+++ bundle.js
@@ -29729,7 +29729,7 @@
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	var createUrl = function createUrl(id) {
-		return 'http://localhost:4000/images/medium/' + id + '.jpg'; //  use  ES2015 template
+		return 'https://white-label-react.herokuapp.com/images/medium/' + id + '.jpg'; //  use  ES2015 template
 	};
 
 	var Results = function (_React$Component) {
